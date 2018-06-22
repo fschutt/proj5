61c30acf1512cedf2676ea15bb47a1ad858bf44f
Felix Schütt <felix.schuett@maps4print.com>
2018-06-22 18:42:55 +0200

    Release 0.1.5

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,13 @@
 //! PROJ.5 defines the 24 standard ellipsoids (such as WGS84, Bessel, etc.),
 //! but you can make your own ellipsoids.
 //!
+//! ## Changelog
+//!
+//! - 0.1.5: Updated to `dyn Trait` syntax to prepare for possible Rust 2018 breakage
+//!          This version will only compile on Rust 1.27 or higher, use 0.1.4 for older
+//!          compilers
+//! - 0.1.4: Fixed large mathematical mistake in UTM projection
+//!
 //! ## Usage
 //!
 //! ```rust
